6a2f8a688472a7438a067a01c05cee5d3ae69298
Scott Ford <smford22@gmail.com>
2016-09-27 11:06:27 -0700

    Initial commit

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,14 @@
 # Recipe:: default
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
+package 'httpd' do
+  action :install
+end
+
+file '/var/www/html/index.html' do
+  content '<h1>Hello EA!!!</h1>'
+end
+
+service 'httpd' do
+  action [:start, :enable]
+end
